71d632eed4cc5471892cc85303de6b8207b12340
kazuki04 <kazukik0402@gmail.com>
2022-03-06 20:59:11 +0900

    Refactoring

--- resources/js/reviewing_infomation.js
+++ resources/js/reviewing_infomation.js
@@ -1,34 +1,36 @@
 window.addEventListener("load", () => {
   let days_array = [
-    { days_word: "One day before", days: 1 },
-    { days_word: "Three days before", days: 3 },
-    { days_word: "Ten days before", days: 10 },
-    { days_word: "Fifteen days before", days: 15 },
-    { days_word: "Twenty days before", days: 20 },
+    { display_words: "One day before", days: 1 },
+    { display_words: "Three days before", days: 3 },
+    { display_words: "Ten days before", days: 10 },
+    { display_words: "Fifteen days before", days: 15 },
+    { display_words: "Twenty days before", days: 20 },
   ];
 
   class DateFormatter {
-    constructor(days) {
-      this.date = new Date().setDate(new Date().getDate() - days);
+    constructor(days_object) {
+      this.date = new Date().setDate(new Date().getDate() - days_object.days);
+      this.display_words = days_object.display_words;
     }
 
-    insertDays(days_word) {
+    insertDisplayWords() {
       let li_element = document.createElement("li");
       li_element.className = "days";
 
       let days_word_element = document.createElement("p");
       days_word_element.className = "days_word";
-      days_word_element.innerText = days_word;
+      days_word_element.innerText = this.display_words;
 
       let days_display_element = document.createElement("p");
       days_display_element.innerText = moment(this.date).format("YYYY/MM/DD");
+
       li_element.appendChild(days_word_element);
       li_element.appendChild(days_display_element);
       document.getElementById("reviewing_information").appendChild(li_element);
     }
   }
 
-  days_array.forEach((element) => {
-    new DateFormatter(element.days).insertDays(element.days_word);
+  days_array.forEach((days_object) => {
+    new DateFormatter(days_object).insertDisplayWords();
   });
 });
